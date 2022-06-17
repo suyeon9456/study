f80eafb9d09e43851ef80beecb49f615ebf76d94
suyeon <suyeon9456@naver.com>
2022-06-17 16:24:40 +0900

    feat: Button component

--- react18-ts-todolist/front/src/component/atoms/Button/index.tsx
+++ react18-ts-todolist/front/src/component/atoms/Button/index.tsx
@@ -1,8 +1,23 @@
 import { BasicButton } from "./style";
 
-const Button = () => {
+export const Size = {
+  SMALL: 'SMALL',
+  MEDIUM: 'MEDIUM',
+  LARGE: 'LARGE',
+} as const;
+
+export const Type = {
+  PRIMARY: 'PRIMARY',
+  BASIC: 'BASIC',
+  TEXT: 'TEXT',
+} as const;
+
+type TSize = typeof Size[keyof typeof Size];
+type TType = typeof Type[keyof typeof Type];
+
+const Button = ({ label, size, type, onClick }: { label: string; size: TSize, type: TType, onClick: () => void; }) => {
   return (
-    <BasicButton>test</BasicButton>
+    <BasicButton className={type}>{label}</BasicButton>
   )
 };
 
--- react18-ts-todolist/front/src/component/atoms/Button/style.ts
+++ react18-ts-todolist/front/src/component/atoms/Button/style.ts
@@ -1,5 +1,22 @@
 import styled, { css } from 'styled-components';
 
 export const BasicButton = styled.button`
-  
+  /* font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; */
+  font-weight: 700;
+  border: 2px solid #31d4d5;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+  background-color: #ffffff;
+  box-sizing: border-box;
+
+  font-size: 12px;
+  padding: 10px 16px;
+
+  &:hover {
+    border: 2px solid linear-gradient(to right,#d0de31 0%,#31d4d5 99%);
+    color: #ffffff;
+    background-image: linear-gradient(to right,#d0de31 0%,#31d4d5 99%);
+  }
 `;
